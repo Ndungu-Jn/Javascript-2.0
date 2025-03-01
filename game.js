17722fc25b73455fe1c3f1277726a164354198af
Ndungu-Jn <ndungulink@gmail.com>
2025-03-01 07:24:37 +0300

    added the logic or

--- game.js
+++ game.js
@@ -12,11 +12,11 @@ const shouldWePlay = prompt("Do you want to play? ")
 if (shouldWePlay.toLowerCase() === "yes") {
 
     //game logic
-    const leftOrRight = prompt("You enter a maze, do you want to go left or right? ").toLowerCase
+    const leftOrRight = prompt("You enter a maze, do you want to go left or right? ")
     if (leftOrRight === "left"){
         console.log("You go left and see a bridge? ")
         const cross =prompt("Do you want to cross the bridge? ").toLowerCase()
-        if (cross === "yes"){
+        if (cross === "yes" || cross ==="y"){
             console.log("You cross but the bridge was weak and broke and  you fell. You lost...")
         }else{
             console.log("Good choice... You win")
